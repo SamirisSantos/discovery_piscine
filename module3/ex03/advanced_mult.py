37e3e7b4c754718c66f5e6d5327057a80abbcce2
Samiris De Araujo Santos <samde-ar@c1r11s3.42porto.com>
2026-07-07 15:44:21 +0100

    correct modulo3

--- module3/ex03/advanced_mult.py
+++ module3/ex03/advanced_mult.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
+# for i in range(0, 11):
+#     print(f"Table of {i} : ", end = "")
+#     for j in range(0, 11):
+#         print(i * j, end = " ")
+#     print()
 
-for i in range(0, 11):
+i = 0
+while i <= 10:
     print(f"Table of {i} : ", end = "")
-    for j in range(0, 11):
+    j = 0
+    while j <= 10:
         print(i * j, end = " ")
+        j += 1
+    i += 1
     print()
