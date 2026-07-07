34d6af5ba76e580b5e79bd9805a90c94f2c286b2
Samiris De Araujo Santos <samde-ar@c1r11s3.42porto.com>
2026-07-07 16:14:35 +0100

    update module3

--- module3/ex00/to25.py
+++ module3/ex00/to25.py
@@ -5,5 +5,8 @@ number = int(input("Enter a number less than 25 \n"))
 if number > 25:
     print("Error")
 else:
-    for i in range(number, 26):
-         print(f"Inside the loop, my variable is {i}")
+    while number <= 25:
+        print(f"Inside the loop, my variable is {number}")
+        number += 1
+    # for i in range(number, 26):
+    #      print(f"Inside the loop, my variable is {i}")
--- module3/ex01/multiplication_table.py
+++ module3/ex01/multiplication_table.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 number = int(input("Enter a number \n"))
+i = 0
+while i < 10:
+    print(f"{i} x {number} = {i * number}")
+    i += 1
 
-for i in range(0, 10):
-         print(f"{i} x {number} = {i * number}")
+# for i in range(0, 10):
+#          print(f"{i} x {number} = {i * number}")
