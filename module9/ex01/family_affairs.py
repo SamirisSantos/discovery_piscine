8e51d15e7b54943ef7350e9d1045e05969a7f06c
Samiris De Araujo Santos <samde-ar@c1r12s1.42porto.com>
2026-07-09 13:34:58 +0100

    ex01 modulo9

--- module9/ex01/family_affairs.py
+++ module9/ex01/family_affairs.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+def find_the_redheads(family):
+
+
+dupont_family = {
+    "florian": "red",
+    "marie": "blond",
+    "virginie": "brunette",
+    "david": "red",
+    "franck": "red"
+}
+print(find_the_redheads(dupont_family))
