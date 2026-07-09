d826fb75e070afd8be440a65b49ea2836ff8b63a
Samiris De Araujo Santos <samde-ar@c1r12s1.42porto.com>
2026-07-09 13:15:52 +0100

    ex00 modulo9

---
+++ module9/ex00/your_namebook.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+def array_of_names(names):
+    return [f"{key.capitalize()} {value.capitalize()}" for key, value in names.items()]
+
+persons = {
+    "jean": "valjean",
+    "grace": "hopper",
+    "xavier": "niel",
+    "fifi": "brindacier"
+}
+
+print(array_of_names(persons))
+
+
+# .items()
+# O Python olha para o dicionário names 
+# que você enviou e usa o método .items().
